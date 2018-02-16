b24f682e3dd559b0cdc6f949a174f68e4aaf36d0
Dave Mac <ihategates@hotmail.com>
2018-02-16 17:00:43 +1100

    Pushing around the facenet code into the helpers

--- PythonService/Helpers/align_dataset.py
+++ PythonService/Helpers/align_dataset.py
@@ -28,15 +28,15 @@ from __future__ import print_function
 from scipy import misc
 import sys
 import os
-import argparse
+#import argparse
 import tensorflow as tf
 import numpy as np
-import facenet
-import align.detect_face
+import Helpers.facenet as facenet
+import Helpers.detect_face as detect_face
 import random
 from time import sleep
 
-def main(args):
+def align_faces(args):
     sleep(random.random())
     output_dir = os.path.expanduser(args.output_dir)
     if not os.path.exists(output_dir):
@@ -52,7 +52,7 @@ def main(args):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
-            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
     
     minsize = 20 # minimum size of face
     threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
@@ -93,7 +93,7 @@ def main(args):
                             img = facenet.to_rgb(img)
                         img = img[:,:,0:3]
     
-                        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+                        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
                         nrof_faces = bounding_boxes.shape[0]
                         if nrof_faces>0:
                             det = bounding_boxes[:,0:4]
@@ -138,22 +138,22 @@ def main(args):
     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
             
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
-    parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
-    parser.add_argument('--image_size', type=int,
-        help='Image size (height, width) in pixels.', default=182)
-    parser.add_argument('--margin', type=int,
-        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
-    parser.add_argument('--random_order', 
-        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
-    parser.add_argument('--gpu_memory_fraction', type=float,
-        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
-    parser.add_argument('--detect_multiple_faces', type=bool,
-                        help='Detect and align multiple faces per image.', default=False)
-    return parser.parse_args(argv)
-
-if __name__ == '__main__':
-    main(parse_arguments(sys.argv[1:]))
+#def parse_arguments(argv):
+#    parser = argparse.ArgumentParser()
+#    
+#    parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
+#    parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
+#    parser.add_argument('--image_size', type=int,
+#        help='Image size (height, width) in pixels.', default=182)
+#    parser.add_argument('--margin', type=int,
+#        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+#    parser.add_argument('--random_order', 
+#        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+#    parser.add_argument('--gpu_memory_fraction', type=float,
+#        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+#    parser.add_argument('--detect_multiple_faces', type=bool,
+#                        help='Detect and align multiple faces per image.', default=False)
+#    return parser.parse_args(argv)
+#
+#if __name__ == '__main__':
+#    main(parse_arguments(sys.argv[1:]))
--- PythonService/trainer.py
+++ PythonService/trainer.py
@@ -1,5 +1,18 @@
 import os
 from shutil import copyfile
+import Helpers.align_dataset as align
+
+class AlignOptions():
+    def __init__(self):
+        self.input_dir = ""
+        self.output_dir = ""
+        self.image_size = 182
+        self.margin = 44
+        self.random_order = False
+        self.gpu_memory_fraction = 1.0
+        self.detect_multiple_faces = False
+        
+        
 
 def train(input_folder_path, model_folder_name):
     print("Input Folder Path:", input_folder_path)
@@ -10,7 +23,30 @@ def train(input_folder_path, model_folder_name):
         return False, "Invalid input folder!"
     
 #    os.listdir(input_folder_path)
+        
+    align.align_faces(AlignOptions())
+        
     
+
+        
+    
+#def parse_arguments(argv):
+#    parser = argparse.ArgumentParser()
+#    
+#    parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
+#    parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
+#    parser.add_argument('--image_size', type=int,
+#        help='Image size (height, width) in pixels.', default=182)
+#    parser.add_argument('--margin', type=int,
+#        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+#    parser.add_argument('--random_order', 
+#        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+#    parser.add_argument('--gpu_memory_fraction', type=float,
+#        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+#    parser.add_argument('--detect_multiple_faces', type=bool,
+#                        help='Detect and align multiple faces per image.', default=False)
+#    return parser.parse_args(argv)
+#
 
     
     return True, ""
