ce248ebf9817506382927151f56ccdf656d0dbbe
Dave Mac <ihategates@hotmail.com>
2018-02-16 19:13:21 +1100

    Duplicate image if there's only 1 to train

--- PythonService/trainer.py
+++ PythonService/trainer.py
@@ -30,6 +30,21 @@ def train(input_folder_path, model_folder_name):
     processed_dir = os.path.join(model_dir, "data")
     
     align.align_faces(AlignOptions(input_folder_path, processed_dir))
+    
+    directories = os.listdir(processed_dir)
+    
+    for d in directories:
+        subdir = os.path.join(processed_dir, d)
+        
+        if os.path.isdir(subdir):
+            files = os.listdir(subdir)
+            
+            if len(files) == 1:
+                file_name_split = os.path.splitext(files[0])
+                file_path_from = os.path.join(subdir, files[0])
+                file_path_to = os.path.join(subdir, file_name_split[0] + "_2" + file_name_split[1])
+                print("Only 1 image found for training... Duplicating ", file_path_from)
+                copyfile(file_path_from, file_path_to)
 
     # TODO: duplicate picture if only 1 exists
     
