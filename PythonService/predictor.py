d97189ceb7f9fcf629f3272a986af86810d40ce6
Dave Mac <ihategates@hotmail.com>
2018-02-18 08:28:09 +1100

    Align predicted image

--- PythonService/predictor.py
+++ PythonService/predictor.py
@@ -2,16 +2,22 @@ import os
 import base64
 from mimetypes import guess_extension
 import uuid
+import Helpers.align_dataset as align
+from align_options import AlignOptions
 
 def predict(image):
     train_data_path = "D:\_GithubTest\FacialRecognitionPrototype\data"
     temp_path = os.path.join(train_data_path, "temp")
+    temp_data_path = os.path.join(temp_path, "data")
     
     if not os.path.exists(train_data_path):
         return False, "Training Data Path not found"
     
     if not os.path.exists(temp_path):
         os.makedirs(temp_path)
+
+    if not os.path.exists(temp_data_path):
+        os.makedirs(temp_data_path)
     
     print("Convert base64 image to temp file")
 
@@ -28,13 +34,13 @@ def predict(image):
     image = image_split[1]
 
     file_name = str(uuid.uuid4()) + extension
-    file_path = os.path.join(temp_path, file_name)
+    file_path = os.path.join(temp_data_path, file_name)
     imgdata = base64.b64decode(image)
     
     with open(file_path, "wb") as fh:
         fh.write(imgdata)
         
-    
-    
+    print("Align image to work with classifier")
+    align.align_faces(AlignOptions(temp_path, os.path.join(train_data_path, "temp_predict")))
     
     return True, ""
--- PythonService/trainer.py
+++ PythonService/trainer.py
@@ -2,18 +2,8 @@ import os
 from shutil import copyfile
 import Helpers.align_dataset as align
 import classifier
-
-class AlignOptions():
-    def __init__(self, input_dir, output_dir):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
-        self.image_size = 160
-        self.margin = 32
-        self.random_order = True
-        self.gpu_memory_fraction = 0.25
-        self.detect_multiple_faces = False
+from align_options import AlignOptions
         
-
 def train(input_folder_path, model_folder_name):
     print("Input Folder Path:", input_folder_path)
     print("Model Folder Name:", model_folder_name)
