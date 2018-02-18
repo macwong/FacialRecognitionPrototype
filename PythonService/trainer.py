855373bd8966a0f6a943eac0450e391b555f0788
Dave Mac <ihategates@hotmail.com>
2018-02-18 15:03:51 +1100

    Fix training

--- PythonService/trainer.py
+++ PythonService/trainer.py
@@ -11,7 +11,7 @@ def train(input_folder_path, model_folder_name):
     
     print("Checking Directories...")
     if os.path.exists(input_folder_path) == False:
-        return False, None, "Invalid input folder!"
+        return False, "Invalid input folder!"
     
     
     print("Aligning faces...")
@@ -19,7 +19,7 @@ def train(input_folder_path, model_folder_name):
     model_dir = os.path.join(train_data_path, model_folder_name)
     processed_dir = os.path.join(model_dir, "data")
     
-    align.align_faces(AlignOptions(input_folder_path, processed_dir))
+    align.align_faces(AlignOptions(input_folder_path, processed_dir, True))
     
     directories = os.listdir(processed_dir)
     
@@ -38,9 +38,9 @@ def train(input_folder_path, model_folder_name):
     
     print("Training...")
     
-    success, predictions, error = classifier.classifier(mode = "TRAIN", 
+    classifier.classifier(mode = "TRAIN", 
            model = "D:\\_GithubTest\\FacialRecognitionPrototype\\data\\facenet_models\\20170512-110547.pb",
            data_dir = processed_dir, 
            classifier_filename = os.path.join(model_dir, "classifier.pkl"))
     
-    return success, predictions, error
+    return True, ""
