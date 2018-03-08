10889c26964dd188fb21670dec9f0723de176677
Dave Mac <ihategates@hotmail.com>
2018-03-09 08:29:52 +1100

    Get the features

--- PythonService/addnewface.py
+++ PythonService/addnewface.py
@@ -1,12 +1,20 @@
 from Helpers import helpers
 from daveglobals import Globals
+from mygraph import MyGraph
 import shutil
+import classifier
+import os
 
-def add(image, model):
+def add(image, model_folder):
     # Create temp image
     helpers.save_temp_face(image)
     
     # Get facenet embeddings
+    model_path = os.path.join(Globals.model_path, model_folder)
+    classifier_file = os.path.join(model_path, "classifier.pkl")
+    
+    features = classifier.get_features(Globals.temp_path, MyGraph(), classifier_file)
+    print(features.emb_array)
     
     # Load model
     
