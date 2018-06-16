39b45f57a3dc83b5aa0258379b9bbfc2922d5ce4
David McCormick <dave.a.mccormick@gmail.com>
2018-06-16 17:12:47 +1000

    Hack is only for svc

--- PythonService/trainer.py
+++ PythonService/trainer.py
@@ -42,19 +42,21 @@ def train(input_folder_path, model_folder_name, model_type):
     align.align_faces(AlignOptions(input_folder_path, processed_dir, my_graph))
     
     directories = os.listdir(processed_dir)
-    
-    for d in directories:
-        subdir = os.path.join(processed_dir, d)
-        
-        if os.path.isdir(subdir):
-            files = os.listdir(subdir)
+
+    # SVC's don't seem to be able to handle only having 1 image for training, so let's create a duplicate    
+    if model_type == "svc":
+        for d in directories:
+            subdir = os.path.join(processed_dir, d)
             
-            if len(files) == 1:
-                file_name_split = os.path.splitext(files[0])
-                file_path_from = os.path.join(subdir, files[0])
-                file_path_to = os.path.join(subdir, file_name_split[0] + "_2" + file_name_split[1])
-                print("Only 1 image found for training... Duplicating ", file_path_from)
-                copyfile(file_path_from, file_path_to)
+            if os.path.isdir(subdir):
+                files = os.listdir(subdir)
+                
+                if len(files) == 1:
+                    file_name_split = os.path.splitext(files[0])
+                    file_path_from = os.path.join(subdir, files[0])
+                    file_path_to = os.path.join(subdir, file_name_split[0] + "_2" + file_name_split[1])
+                    print("Only 1 image found for training... Duplicating ", file_path_from)
+                    copyfile(file_path_from, file_path_to)
     
     print("Training...")
     
