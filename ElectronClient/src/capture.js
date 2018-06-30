3299e7530a6017880cb95d20978ea0eb4f7b5b66
David McCormick <dave.a.mccormick@gmail.com>
2018-07-01 07:46:17 +1000

    Move all the code to viewer.jsx

--- ElectronClient/src/capture.js
+++ ElectronClient/src/capture.js
@@ -1,142 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Viewer from './react/viewer';
-import Globals from './globals';
-import Helpers from './helpers';
-import { ImageProcessor } from './imageprocessor';
 
-const electron = require("electron");
-const { remote } = electron;
 const video = require("./video");
 const fs = require("fs");
 const $ = require("./jquery")
-const { dialog } = electron.remote;
-const path = require('path')
 
-let m_imageProcessor = null;
-
-function getModels($select) {
-    var deferred = $.Deferred();
-
-    $.ajax({
-        url: path.join(Globals.endpoint, "getmodels"),
-        type: "GET",
-        dataType:"json"
-    }).done((result) => {
-        $select.empty();
-
-        for (var i = 0; i < result.models.length; i++) {
-            let $option = $("<option></option>");
-            $option.text(result.models[i]);
-            $option.val(result.models[i]);
-            $select.append($option);
-        }
-
-        deferred.resolve();
-    });
-
-    return deferred.promise();
-}
-
-function initApp(callback) {
-    let $models = $(document).find(".models");
-    let $select = $("<select></select>");
-
-    getModels($select).then(() => {
-        $models.html($select);
-
-        m_imageProcessor.currentModel = $select.find("option:first").val();
-
-        $select.change((e) => {
-            m_imageProcessor.currentModel = $select.val();
-        })
-
-        $('.add-model').on('click', (e) => {
-            e.preventDefault();
-
-            $.get(path.join(__dirname, 'modal.html'), (data) => {
-                let $modal = $(data);
-                let $name = $modal.find(".model-name");
-                let $folderLocation = $modal.find(".folder-location");
-                let $algorithm = $modal.find(".algorithm");
-                let $loading = $modal.find(".loading");
-
-                $(document.body).append($modal);
-
-                setTimeout(() => {
-                    $modal.addClass('is-visible');
-                }, 50);
-
-                $modal.find(".modal-toggle").click((e) => {
-                    $modal.removeClass('is-visible');
-                    
-                    $modal.on('transitionend', (e) => {
-                        //when transition is finished you remove the element.
-                        $modal.remove();
-                    });
-                });
-
-                $modal.find(".choose-folder").click((e) => {
-                    dialog.showOpenDialog(
-                        remote.getCurrentWindow(), {
-                            properties: ['openDirectory']
-                        },
-                        function (filePaths) {
-                            $folderLocation.val(filePaths[0]);
-                        }
-                    );
-                });
-
-                $modal.find(".model-add").click((e) => {
-                    $loading.addClass("is-visible");
-
-                    $.ajax({
-                        url: path.join(Globals.endpoint, "train"),
-                        type: "POST",
-                        data: JSON.stringify({
-                            input_folder_path: $folderLocation.val(),
-                            model_folder_name: $name.val(),
-                            model_type: $algorithm.val()
-                        }),
-                        contentType: "application/json; charset=utf-8",
-                        dataType:"json",
-                        
-                    }).done((result) => {
-                        $loading.removeClass("is-visible");
-                        $modal.removeClass('is-visible');
-                    
-                        $modal.on('transitionend', (e) => {
-                            //when transition is finished you remove the element.
-                            $modal.remove();
-                        });
-
-                        getModels($select).then(() => {
-                            $select.val(m_imageProcessor.currentModel);
-                        });
-                    }).fail((jqXHR, textStatus, errorThrown) => {
-                        $loading.removeClass("is-visible");
-                        let $errorMsg = $modal.find(".error-message");
-                        $errorMsg.text(jqXHR.responseJSON.error);
-                    });
-                });
-            });
-        });
-
-        callback();
-    });
-}
-
-function getVideoStream(videoEl) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-        videoEl.src = URL.createObjectURL(stream);
-
-        videoEl.onloadedmetadata = (e) => {
-            m_imageProcessor.captureImage();
-        };
-    }).catch(() =>  {
-        alert('could not connect stream');
-    });
-}
 
 $(document).ready(() => {
     ReactDOM.render(
@@ -144,131 +13,94 @@ $(document).ready(() => {
         document.getElementById("viewerContainer")
     );
 
-    const videoEl = document.getElementById("video");
-    const canvasEl = document.getElementById("canvas");
-    const $resultsContainer = $(document).find(".resultsContainer");
-    const $history = $(document).find(".history");
-    const $info = $(document).find(".info");
+    // const videoEl = document.getElementById("video");
+    // const canvasEl = document.getElementById("canvas");
+    // const $resultsContainer = $(document).find(".resultsContainer");
+    // const $history = $(document).find(".history");
+    // const $info = $(document).find(".info");
     
-    m_imageProcessor = new ImageProcessor(
-        videoEl, 
-        canvasEl, 
-        $resultsContainer.find(".resultsOverlay"), 
-        document.getElementById('info')
-    );
+    // m_imageProcessor = new ImageProcessor(
+    //     videoEl, 
+    //     canvasEl, 
+    //     $resultsContainer.find(".resultsOverlay"), 
+    //     document.getElementById('info')
+    // );
 
-    initApp(() => {
-        $resultsContainer.find(".resultsContents").text("Loading...");
-        getVideoStream(videoEl);
-    });
+    // initApp(() => {
+    //     $resultsContainer.find(".resultsContents").text("Loading...");
+    //     getVideoStream(videoEl);
+    // });
 
-    $(document).find(".input-checkbox").change((e) => {
-        let $inputCheckbox = $(e.currentTarget);
+    // $(document).find(".input-checkbox").change((e) => {
+    //     let $inputCheckbox = $(e.currentTarget);
 
-        if ($inputCheckbox.is(":checked")) {
-            $history.show();
-            $info.show();
-            m_imageProcessor.verbose = true;
-        }
-        else {
-            $history.hide();
-            $info.hide();
-            m_imageProcessor.verbose = false;
-        }
-    });
+    //     if ($inputCheckbox.is(":checked")) {
+    //         $history.show();
+    //         $info.show();
+    //         m_imageProcessor.verbose = true;
+    //     }
+    //     else {
+    //         $history.hide();
+    //         $info.hide();
+    //         m_imageProcessor.verbose = false;
+    //     }
+    // });
 
-    let $segment = $(".segmented label input[type=radio]");
+    // let $segment = $(".segmented label input[type=radio]");
 
-    $segment.on("change", (e) => {
-        let $option = $(e.currentTarget);
-        let $parent = $option.parent();
+    // $segment.on("change", (e) => {
+    //     let $option = $(e.currentTarget);
+    //     let $parent = $option.parent();
 
-        if($option.is(":checked")) {
-            $parent.siblings().each((sigIndex, sibValue) => {
-                $(sibValue).removeClass("checked");
-            });
+    //     if($option.is(":checked")) {
+    //         $parent.siblings().each((sigIndex, sibValue) => {
+    //             $(sibValue).removeClass("checked");
+    //         });
 
-            $parent.addClass("checked");
+    //         $parent.addClass("checked");
 
-            let $video = $(videoEl);
-            $video.prop("controls", false);
+    //         let $video = $(videoEl);
+    //         $video.prop("controls", false);
 
-            if ($parent.hasClass("option-live") || $parent.hasClass("option-video")) {
-                $video.show();
-                $(canvasEl).hide();
-                m_imageProcessor.isVideo = true;
+    //         if ($parent.hasClass("option-live") || $parent.hasClass("option-video")) {
+    //             $video.show();
+    //             $(canvasEl).hide();
+    //             m_imageProcessor.isVideo = true;
 
-                if ($parent.hasClass("option-live")) {
-                    getVideoStream(videoEl);
-                }
-                else {
-                    $video.addClass("file");
-                    $video.prop("src", "");
-                    $video.removeAttr("src");
-                }
-            }
-            else if ($parent.hasClass("option-image")) {
-                if ($video.hasClass("file")) {
-                    $video.removeClass("file");
+    //             if ($parent.hasClass("option-live")) {
+    //                 getVideoStream(videoEl);
+    //             }
+    //             else {
+    //                 $video.addClass("file");
+    //                 $video.prop("src", "");
+    //                 $video.removeAttr("src");
+    //             }
+    //         }
+    //         else if ($parent.hasClass("option-image")) {
+    //             if ($video.hasClass("file")) {
+    //                 $video.removeClass("file");
 
-                    canvasEl.width = Globals.defaultWidth;
-                    canvasEl.height = Globals.defaultHeight;
-                }
+    //                 canvasEl.width = Globals.defaultWidth;
+    //                 canvasEl.height = Globals.defaultHeight;
+    //             }
                 
-                $video.hide();
-                $video.prop("src", "");
-                $video.removeAttr("src");
-                $(canvasEl).show();
-                m_imageProcessor.isVideo = false;
-            }
-        }
-    });
+    //             $video.hide();
+    //             $video.prop("src", "");
+    //             $video.removeAttr("src");
+    //             $(canvasEl).show();
+    //             m_imageProcessor.isVideo = false;
+    //         }
+    //     }
+    // });
 
-    $(videoEl).click((e) => {
-        if ($(e.currentTarget).hasClass("file")) {
-            openVideo(videoEl, canvasEl, $resultsContainer);
-        }
-    });
+    // $(videoEl).click((e) => {
+    //     if ($(e.currentTarget).hasClass("file")) {
+    //         openVideo(videoEl, canvasEl, $resultsContainer);
+    //     }
+    // });
 
-    $(canvasEl).click((e) => {
-        openImage();
-    });
+    // $(canvasEl).click((e) => {
+    //     openImage();
+    // });
 });
 
-function openVideo(videoEl, canvasEl, $resultsContainer) {
-    dialog.showOpenDialog(
-        remote.getCurrentWindow(),
-        {
-            defaultPath: 'c:/',
-            filters: [
-                { name: 'Images', extensions: ['avi', 'mov', 'mkv', 'mp4'] }
-              ],
-            properties: ['openFile']
-        },
-        function (filePaths) {
-            $(videoEl).prop("controls", true);
-            
-            Helpers.fadeStuff($resultsContainer.find(".resultsOverlay"));
-            m_imageProcessor.captureImage();
-
-            videoEl.src = filePaths[0];
-        }
-    );    
-}
-
-function openImage() {
-    dialog.showOpenDialog(
-        remote.getCurrentWindow(),
-        {
-            defaultPath: 'c:/',
-            filters: [
-                { name: 'Images', extensions: ['jpg', 'png', 'gif'] }
-              ],
-            properties: ['openFile', 'multiSelections']
-        },
-        function (filePaths) {
-            m_imageProcessor.currentImages = filePaths;
-            m_imageProcessor.updateImage();
-        }
-    );
-}
--- ElectronClient/src/react/viewer.jsx
+++ ElectronClient/src/react/viewer.jsx
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 import Controls from './ViewerControls/controls';
+import { ImageProcessor } from '../imageprocessor';
+import Globals from '../globals';
+import Helpers from '../helpers';
+import electron from 'electron';
+import {remote} from 'electron';
+import $ from 'jquery';
+
+const { dialog } = electron.remote;
+const path = require('path')
 
 export default class Viewer extends Component {
     constructor(props) {
@@ -8,9 +17,9 @@ export default class Viewer extends Component {
         this.imageProcessor = null;
         this.videoEl = null;
         this.canvasEl = null;
-        this.resultsOverlay = null;
-        this.history = null;
-        this.info = null;
+        this.$resultsContainer = null;
+        this.$history = null;
+        this.$info = null;
     }
 
     render() {
@@ -24,17 +33,255 @@ export default class Viewer extends Component {
     }
 
     componentDidMount() {
-        // this.videoEl = document.getElementById("video");
-        // this.canvasEl = document.getElementById("canvas");
-        // this.resultsOverlay = document.getElementById("resultsOverlay");
-        // this.history = document.getElementById("history");
-        // this.info = document.getElementById("info");
+        this.videoEl = document.getElementById("video");
+        this.canvasEl = document.getElementById("canvas");
+        this.$resultsContainer = $(document.getElementById("resultsContainer"));
+        this.$history = $(document.getElementById("history"));
+        this.$info = $(document.getElementById("info"));
 
-        // this.imageProcessor = new ImageProcessor(
-        //     this.videoEl, 
-        //     this.canvasEl, 
-        //     this.resultsOverlay, 
-        //     this.info
-        // );
+        this.imageProcessor = new ImageProcessor(
+            this.videoEl, 
+            this.canvasEl, 
+            this.$resultsContainer.find(".resultsOverlay"), 
+            this.$info
+        );
+
+        this.initApp(() => {
+            this.$resultsContainer.find(".resultsContents").text("Loading...");
+            this.getVideoStream(this.videoEl);
+        });
+    
+        $(document).find(".input-checkbox").change((e) => {
+            let $inputCheckbox = $(e.currentTarget);
+    
+            if ($inputCheckbox.is(":checked")) {
+                this.$history.show();
+                this.$info.show();
+                this.imageProcessor.verbose = true;
+            }
+            else {
+                this.$history.hide();
+                this.$info.hide();
+                this.imageProcessor.verbose = false;
+            }
+        });
+    
+        let $segment = $(".segmented label input[type=radio]");
+    
+        $segment.on("change", (e) => {
+            let $option = $(e.currentTarget);
+            let $parent = $option.parent();
+    
+            if($option.is(":checked")) {
+                $parent.siblings().each((sigIndex, sibValue) => {
+                    $(sibValue).removeClass("checked");
+                });
+    
+                $parent.addClass("checked");
+    
+                let $video = $(this.videoEl);
+                $video.prop("controls", false);
+    
+                if ($parent.hasClass("option-live") || $parent.hasClass("option-video")) {
+                    $video.show();
+                    $(this.canvasEl).hide();
+                    this.imageProcessor.isVideo = true;
+    
+                    if ($parent.hasClass("option-live")) {
+                        this.getVideoStream(this.videoEl);
+                    }
+                    else {
+                        $video.addClass("file");
+                        $video.prop("src", "");
+                        $video.removeAttr("src");
+                    }
+                }
+                else if ($parent.hasClass("option-image")) {
+                    if ($video.hasClass("file")) {
+                        $video.removeClass("file");
+    
+                        this.canvasEl.width = Globals.defaultWidth;
+                        this.canvasEl.height = Globals.defaultHeight;
+                    }
+                    
+                    $video.hide();
+                    $video.prop("src", "");
+                    $video.removeAttr("src");
+                    $(this.canvasEl).show();
+                    this.imageProcessor.isVideo = false;
+                }
+            }
+        });
+    
+        $(this.videoEl).click((e) => {
+            if ($(e.currentTarget).hasClass("file")) {
+                this.openVideo(this.videoEl, this.canvasEl, this.$resultsContainer);
+            }
+        });
+    
+        $(this.canvasEl).click((e) => {
+            this.openImage();
+        });
+    }
+
+    initApp(callback) {
+        let $models = $(document).find(".models");
+        let $select = $("<select></select>");
+    
+        this.getModels($select).then(() => {
+            $models.html($select);
+    
+            this.imageProcessor.currentModel = $select.find("option:first").val();
+    
+            $select.change((e) => {
+                this.imageProcessor.currentModel = $select.val();
+            })
+    
+            $('.add-model').on('click', (e) => {
+                e.preventDefault();
+    
+                $.get(path.join(__dirname, 'modal.html'), (data) => {
+                    let $modal = $(data);
+                    let $name = $modal.find(".model-name");
+                    let $folderLocation = $modal.find(".folder-location");
+                    let $algorithm = $modal.find(".algorithm");
+                    let $loading = $modal.find(".loading");
+    
+                    $(document.body).append($modal);
+    
+                    setTimeout(() => {
+                        $modal.addClass('is-visible');
+                    }, 50);
+    
+                    $modal.find(".modal-toggle").click((e) => {
+                        $modal.removeClass('is-visible');
+                        
+                        $modal.on('transitionend', (e) => {
+                            //when transition is finished you remove the element.
+                            $modal.remove();
+                        });
+                    });
+    
+                    $modal.find(".choose-folder").click((e) => {
+                        dialog.showOpenDialog(
+                            remote.getCurrentWindow(), {
+                                properties: ['openDirectory']
+                            },
+                            (filePaths) => {
+                                $folderLocation.val(filePaths[0]);
+                            }
+                        );
+                    });
+    
+                    $modal.find(".model-add").click((e) => {
+                        $loading.addClass("is-visible");
+    
+                        $.ajax({
+                            url: path.join(Globals.endpoint, "train"),
+                            type: "POST",
+                            data: JSON.stringify({
+                                input_folder_path: $folderLocation.val(),
+                                model_folder_name: $name.val(),
+                                model_type: $algorithm.val()
+                            }),
+                            contentType: "application/json; charset=utf-8",
+                            dataType:"json",
+                            
+                        }).done((result) => {
+                            $loading.removeClass("is-visible");
+                            $modal.removeClass('is-visible');
+                        
+                            $modal.on('transitionend', (e) => {
+                                //when transition is finished you remove the element.
+                                $modal.remove();
+                            });
+    
+                            this.getModels($select).then(() => {
+                                $select.val(this.imageProcessor.currentModel);
+                            });
+                        }).fail((jqXHR, textStatus, errorThrown) => {
+                            $loading.removeClass("is-visible");
+                            let $errorMsg = $modal.find(".error-message");
+                            $errorMsg.text(jqXHR.responseJSON.error);
+                        });
+                    });
+                });
+            });
+    
+            callback();
+        });
+    }
+    
+    getVideoStream(videoEl) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            videoEl.src = URL.createObjectURL(stream);
+    
+            videoEl.onloadedmetadata = (e) => {
+                this.imageProcessor.captureImage();
+            };
+        }).catch(() =>  {
+            alert('could not connect stream');
+        });
+    }
+    
+    getModels($select) {
+        var deferred = $.Deferred();
+    
+        $.ajax({
+            url: path.join(Globals.endpoint, "getmodels"),
+            type: "GET",
+            dataType:"json"
+        }).done((result) => {
+            $select.empty();
+    
+            for (var i = 0; i < result.models.length; i++) {
+                let $option = $("<option></option>");
+                $option.text(result.models[i]);
+                $option.val(result.models[i]);
+                $select.append($option);
+            }
+    
+            deferred.resolve();
+        });
+    
+        return deferred.promise();
+    }
+
+    openVideo(videoEl, canvasEl, $resultsContainer) {
+        dialog.showOpenDialog(
+            remote.getCurrentWindow(),
+            {
+                defaultPath: 'c:/',
+                filters: [
+                    { name: 'Images', extensions: ['avi', 'mov', 'mkv', 'mp4'] }
+                  ],
+                properties: ['openFile']
+            },
+            (filePaths) => {
+                $(videoEl).prop("controls", true);
+                
+                Helpers.fadeStuff($resultsContainer.find(".resultsOverlay"));
+                this.imageProcessor.captureImage();
+    
+                videoEl.src = filePaths[0];
+            }
+        );    
+    }
+    
+    openImage() {
+        dialog.showOpenDialog(
+            remote.getCurrentWindow(),
+            {
+                defaultPath: 'c:/',
+                filters: [
+                    { name: 'Images', extensions: ['jpg', 'png', 'gif'] }
+                  ],
+                properties: ['openFile', 'multiSelections']
+            },
+            (filePaths) => {
+                this.imageProcessor.currentImages = filePaths;
+                this.imageProcessor.updateImage();
+            }
+        );
     }
 }
