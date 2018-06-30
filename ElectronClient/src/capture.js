5de0f6e2189eee8588daafbaa3fbbacab9057cbc
David McCormick <dave.a.mccormick@gmail.com>
2018-07-01 08:28:00 +1000

    Don't need to wait for document ready

--- ElectronClient/src/capture.js
+++ ElectronClient/src/capture.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './react/app';
-import $ from 'jquery';
 
-$(document).ready(() => {
-    ReactDOM.render(
-        <App />,
-        document.getElementById("container")
-    );
-});
+ReactDOM.render(
+    <App />,
+    document.getElementById("container")
+);
 
